926c03c3a9a5b276cbe11de4117d7032d2412aee
danielmweb <ldanielmoreno-2014@outlook.com>
2022-10-10 14:09:52 -0300

    added visual feedback for transition between projects

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: [
+				{
+					loader: "@svgr/webpack",
+					options: {
+						svgo: false
+					}
+				}
+			]
+		});
 
-module.exports = nextConfig
+		return config;
+	}
+};
+
+module.exports = nextConfig;
--- pages/projects/[project].jsx
+++ pages/projects/[project].jsx
@@ -4,11 +4,10 @@ import styles from "../../styles/pages/project.module.scss";
 import Link from "next/link";
 import projectsData from "../../projects-data.json";
 import { useEffect, useState } from "react";
+import ArrowIcon from "../../public/arrow-horizontal.svg";
 
 export default function Project({ project, projects }) {
 	const [currentIndex, setCurrentIndex] = useState(findCurrentProjectIndex);
-
-	// console.log(currentIndex);
 
 	function findCurrentProjectIndex() {
 		return projects.findIndex((p) => p.url == project.url);
@@ -26,6 +25,24 @@ export default function Project({ project, projects }) {
 	let nextIndex = isNextValid ? currentIndex + 1 : projects.length - 1;
 	let nextProject = projects[nextIndex];
 
+	function showSpinner() {
+		let spinner = document.querySelector("div[data-spinner]");
+		spinner.style.opacity = 1;
+	}
+
+	function hideSpinner() {
+		let spinner = document.querySelector("div[data-spinner]");
+		spinner.style.opacity = 0;
+	}
+
+	useEffect(() => {
+		let iframe = document.querySelector("iframe");
+
+		if (iframe.contentDocument.querySelector("body")) {
+			hideSpinner();
+		}
+	}, []);
+
 	return (
 		<div className={styles.project}>
 			<Head>
@@ -37,6 +54,11 @@ export default function Project({ project, projects }) {
 						return (
 							<Link href={projectData.url}>
 								<h3
+									onClick={() => {
+										if (projectData.url != project.url) {
+											showSpinner();
+										}
+									}}
 									key={projectData.url}
 									data-active={(projectData.url == project.url).toString()}
 									className={styles.project_link}
@@ -49,7 +71,10 @@ export default function Project({ project, projects }) {
 				</div>
 				<div className={styles.project__text}>
 					<Link href={`/projects/${prevProject.url}`}>
-						<img
+						<ArrowIcon
+							onClick={() => {
+								showSpinner();
+							}}
 							className={styles.arrow__left}
 							src="/arrow-horizontal.svg"
 							alt=""
@@ -61,7 +86,10 @@ export default function Project({ project, projects }) {
 						<p className={styles.project__description}>{project.description}</p>
 					</div>
 					<Link href={`/projects/${nextProject.url}`}>
-						<img
+						<ArrowIcon
+							onClick={() => {
+								showSpinner();
+							}}
 							className={styles.arrow__right}
 							src="/arrow-horizontal.svg"
 							alt=""
@@ -79,18 +107,36 @@ export default function Project({ project, projects }) {
 						);
 					})}
 				</div>
+
 				<div className={styles.project__container}>
 					<Link href={`/projects/${prevProject.url}`}>
-						<img
+						<ArrowIcon
+							onClick={() => {
+								showSpinner();
+							}}
 							className={styles.arrow__left}
 							src="/arrow-horizontal.svg"
 							alt=""
 							data-valid={isPrevValid.toString()}
 						/>
 					</Link>
-					<iframe src={`../../projects/${project.url}/index.html`}></iframe>
+					<div className={styles.project__iframe_container}>
+						<div data-spinner className={styles.spinner}>
+							<div className={styles.spinner__element}></div>
+						</div>
+						<iframe
+							onLoad={(e) => {
+								hideSpinner();
+							}}
+							src={`../../projects/${project.url}/index.html`}
+						></iframe>
+					</div>
+
 					<Link href={`/projects/${nextProject.url}`}>
-						<img
+						<ArrowIcon
+							onClick={() => {
+								showSpinner();
+							}}
 							className={styles.arrow__right}
 							src="/arrow-horizontal.svg"
 							alt=""
